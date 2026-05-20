b8b74a069e28a8596146bceea05382bd0776713a
garciarodriguezclaudiamaria-ai <garciarodriguezclaudiamaria@gmail.com>
2026-05-20 16:07:07 -0600

    Add files via upload

---
+++ asignacion.py
@@ -0,0 +1,46 @@
+x,y,z = "piña", "uva", "fresa"
+print(x)
+print(y)
+print(z)
+print("")
+x=y=z = "naranja"
+print(x)
+print(y)
+print(z)
+print("")
+fruta = ["piña", "uva", "fresa", "limon"]
+x,y,z, a = fruta
+print(x)
+print(y)
+print(z)
+print(a)
+
+x, y ,z = "Python", "is" ,"awesome"
+print(x, y, z)
+print("")
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+print("")
+x = 5
+y = 10
+print(x + y)
+print(x , y)
+print("")
+x = 5
+y = "John"
+print(x, y)
+
+x=4
+print(type(x))
+x = "Migo"
+print(x)
+print(type(x))
+x = 10.1
+print(type(x))
+x= True
+print(type(x))
+x=[1,2,3]
+print(type(x))
+
